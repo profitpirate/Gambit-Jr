407ece1e2b1f46ce866db222c79cf52253caca0b
profitpirate <joshua.ayorinde@yahoo.com>
2026-08-29 21:54:21 +0100

    feat(discord): activate calls-first product policy

--- src/memecoin_bot/discord/__init__.py
+++ src/memecoin_bot/discord/__init__.py
@@ -1,3 +1,6 @@
 from .notifier import DiscordNotifier, NullNotifier
+from .product_policy import install_discord_product_policy
+
+install_discord_product_policy()
 
 __all__ = ["DiscordNotifier", "NullNotifier"]
